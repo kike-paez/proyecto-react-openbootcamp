7cd042a80e92a6bac30bb577513e28eab195aec1
Kike Paez <enrique.paezpozo.com>
2023-04-29 16:08:40 +0200

    Añadidos ejemplos del uso de Hooks

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 // import Greeting from './components/pure/greeting';
 // import GreetingF from './components/pure/greetingF';
 // import ContactoF from './components/pure/contactoF';
-import TaskListComponent from './components/containers/task_list';
+// import TaskListComponent from './components/containers/task_list';
+// import Ejemplo1 from './hooks/Ejemplo1';
+// import Ejemplo2 from './hooks/Ejemplo2';
+// import MiComponenteConContexto from './hooks/Ejemplo3';
+import Ejemplo4 from './hooks/Ejemplo4';
 
 function App() {
   return (
@@ -15,7 +19,15 @@ function App() {
         {/* Componente de ejemplo funcional */}
         {/* <GreetingF name="Kike"></GreetingF> */}
         {/* Componente de Listado de Tareas */}
-        <TaskListComponent></TaskListComponent>
+        {/* <TaskListComponent></TaskListComponent> */}
+        {/* Ejemplos de uso de Hooks */}
+        {/* <Ejemplo1></Ejemplo1> */}
+        {/* <Ejemplo2></Ejemplo2> */}
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+        <Ejemplo4 nombre='Kike'>
+          {/* Todo lo que hay aquí dentro es tratado como props.children */}
+          <h3>Contenido del props.children</h3>
+        </Ejemplo4>
       </header>
     </div>
   );
