fa372e2d4f6c185c00945b9ed21398468ebdc39f
Kike Paez <enrique.paezpozo@gmail.com>
2023-05-02 14:08:23 +0200

    Archivos task y task_list actualizados con hooks

--- src/components/containers/task_list.jsx
+++ src/components/containers/task_list.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Task } from '../../models/task.class';
 import { LEVELS } from '../../models/levels.enum';
 import TaskComponent from '../pure/task';
@@ -7,8 +7,21 @@ import TaskComponent from '../pure/task';
 const TaskListComponent = () => {
 
     const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL);
+    
+    // Estado del componente
+    const [tasks, setTasks] = useState(defaultTask);
+    const [loading, setLoading] = useState(true);
 
-    const changeState = (id) => {
+    // Control del ciclo de vida del componente
+    useEffect(() => {
+        console.log("Task State has been modified");
+        setLoading(false);
+        return () => {
+            console.log("TaskList component is going to unmount")
+        };
+    }, [tasks]);
+    
+    const changeCompleted = (id) => {
         console.log('TODO: Cambiar estado de una tarea')
     }
 
